e9631356fdef61fa7df09a24b55d9b2b13062438
Oliver Iyer <oliver.iyer@doctolib.com>
2025-09-28 21:23:12 +0200

    fix

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py migrate
-web: gunicorn warhammer_list_parser.wsgi --log-file -
--- warhammer_list_parser/settings.py
+++ warhammer_list_parser/settings.py
@@ -30,10 +30,6 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
-# Production settings (Heroku legacy support)
-if 'DYNO' in os.environ:
-    DEBUG = False
-    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
