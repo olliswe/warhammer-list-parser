ab04d00cd89385ea061c6a0229f32ebd9ee291f8
Oliver Iyer <oliveriyer@gmail.com>
2025-10-16 13:11:33 +0200

    fix request line

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Running database migrations..."
 python manage.py migrate --noinput
 
 echo "Starting gunicorn..."
-exec gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 2 warhammer_list_parser.wsgi:application
+exec gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 2 --limit-request-line 0 warhammer_list_parser.wsgi:application
