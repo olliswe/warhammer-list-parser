fe5cd5863ff4fd503794678402611f19af11d087
Oliver Iyer <oliver.iyer@doctolib.com>
2025-10-22 12:51:35 +0200

    fix

--- react-frontend/vite.config.ts
+++ react-frontend/vite.config.ts
@@ -5,13 +5,14 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/static/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    outDir: '../staticfiles/react',
+    outDir: '../build',
     emptyOutDir: true,
   },
   server: {
