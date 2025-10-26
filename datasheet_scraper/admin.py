6157bcb22b1f25b00f3cb2d37e3968e258abf4fa
Oliver Iyer <oliver.iyer@doctolib.com>
2025-10-26 19:56:35 +0100

    admin stuff

--- datasheet_scraper/admin.py
+++ datasheet_scraper/admin.py
@@ -7,6 +7,7 @@ from django.db.models import JSONField
 @admin.register(FactionJson)
 class FactionJsonAdmin(admin.ModelAdmin):
     list_display = ["faction_name", "faction_id"]
+    readonly_fields = ["created_at", "updated_at"]
 
     formfield_overrides = {
         JSONField: {"widget": JSONEditorWidget},
@@ -16,6 +17,7 @@ class FactionJsonAdmin(admin.ModelAdmin):
 @admin.register(DetachmentJson)
 class DetachmentJson(admin.ModelAdmin):
     list_display = ["detachment_id", "detachment_name", "faction_id"]
+    readonly_fields = ["created_at", "updated_at"]
 
     formfield_overrides = {
         JSONField: {"widget": JSONEditorWidget},
@@ -25,6 +27,7 @@ class DetachmentJson(admin.ModelAdmin):
 @admin.register(DatasheetJson)
 class DatasheetJson(admin.ModelAdmin):
     list_display = ["datasheet_id", "datasheet_name", "faction_id"]
+    readonly_fields = ["created_at", "updated_at"]
 
     formfield_overrides = {
         JSONField: {"widget": JSONEditorWidget},
